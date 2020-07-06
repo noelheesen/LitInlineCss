a6124e45f7ed99ed84f418cc6d9f897389d5cf27
Noël Heesen <noelheesen@gmail.com>
2020-07-06 16:34:09 +0200

    chore: project setup
    Add Typescript, eslint, prettier, commitlint

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  quoteProps: 'preserve',
+  tabs: false,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  semi: false,
+  singleQuote: true
+}
